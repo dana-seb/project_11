3e0d63647bc6fd5901f7401cb55d3ab2910e8072
Dana S <dana.s.lewis@gmail.com>
2023-09-18 17:31:02 -0400

    add smaema

--- app/nc_tutorials/urls.py
+++ app/nc_tutorials/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tutorials.urls')),
     path('', include('users.urls')),
+    path('', include('smaema.urls')),
 ]
---
+++ app/smaema/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmaemaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smaema'
